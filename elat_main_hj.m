27fae763660823da33f1a88ffd6f9b0624e1f683
c1745595 <krzeminskidk@cardiff.ac.uk>
2018-04-05 13:55:48 +0100

    function for energies calc added

---
+++ elat_main_hj.m
@@ -0,0 +1,33 @@
+function [Ecalc, LocalMinIndex] = elat_main_hj(h, J, E)
+% modification of elat main
+%
+if nargin < 3
+    E = mfunc_Energy(h, J);
+end
+Ecalc = E;
+%% Calculate Local Minima
+nodeNumber = log2(length(E));
+[LocalMinIndex, BasinGraph, AdjacentList] = mfunc_LocalMin(nodeNumber, E);
+
+for i=1:size(LocalMinIndex)
+    fprintf('LocalMinimum %d : state %d \n', i, LocalMinIndex(i));
+end
+
+save('BasinGraph.mat', 'BasinGraph')
+%% Calculate Energy path by Dijkstra
+
+[Cost, Path] = mfunc_DisconnectivityGraph(E, LocalMinIndex, AdjacentList);
+
+%% Show activity pattern
+% set the Name of signal before calling functiion by hand.
+%Name = {'right ant thal', 'left ant thal', 'dACC/msFC', 'right al/fO',...
+%    'left al/fO', 'right aPFC', 'left aPFC'};
+% if fRoi
+%     Name = flipud(importdata(RoiNameFile));
+% else
+%     Name = [];
+% end
+% Name = [];
+Name = [];
+vectorList = mfunc_VectorList(nodeNumber);
+mfunc_ActivityMap(vectorList, LocalMinIndex, Name);
